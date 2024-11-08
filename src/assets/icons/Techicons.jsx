fa2be38676d04fa72bec9cd6ec519752709e0ae5
rameshyaazhnila <rameshyaazhnila@gmail.com>
2024-11-08 19:52:33 +0530

    aos alter

--- src/assets/icons/Techicons.jsx
+++ src/assets/icons/Techicons.jsx
@@ -37,19 +37,19 @@ function Techicons() {
     AOS.init({
       duration: 1000, 
       once: false, 
-      offset: 150, 
+      offset: 90, 
     });
   }, []);
   return (
-    <div className='flex justify-center'>
+    <div className='flex justify-center' data-aos="fade-left">
       <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 gap-4 p-4 place-items-center">
-        <FaHtml5 size={40} color="#E34F26" className='hover:scale-125 transition-transform duration-150' data-aos="fade-left"/>
-        <FaCss3Alt size={40} color="#1572B6" className='hover:scale-125 transition-transform duration-150' data-aos="fade-left"/>
+        <FaHtml5 size={40} color="#E34F26" className='hover:scale-125 transition-transform duration-150'/>
+        <FaCss3Alt size={40} color="#1572B6" className='hover:scale-125 transition-transform duration-150'/>
         <FaJs size={40} color="#F7DF1E" className='hover:scale-125 transition-transform duration-150' data-aos="fade-left"/>
         <FaReact size={40} color="#61DBFB" className='hover:scale-125 transition-transform duration-150' data-aos="fade-right"/>
         <SiTailwindcss size={40} color="#38B2AC" className='hover:scale-125 transition-transform duration-150' data-aos="fade-left"/>
         <GrNode size={40} color="green" className='hover:scale-125 transition-transform duration-150' data-aos="fade-right"/>
-        <img src="/express.png" alt="Express.js" style={{ width: 40, height: 40 }} className='hover:scale-125 transition-transform duration-150' data-aos="fade-left"/> {/* Express logo */}
+        <img src="/express.png" alt="Express.js" style={{ width: 40, height: 40 }} className='hover:scale-125 transition-transform duration-150'/> 
         <SiMongodb size={40} color="#47A248" className='hover:scale-125 transition-transform duration-150' data-aos="fade-right"/>
         <FaGithub size={40} color="black" className='hover:scale-125 transition-transform duration-150' data-aos="fade-left"/>
       </div>
